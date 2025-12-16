7f05f4a62a0b57f2c98eaaf9164cd12d4a0c2a50
gavin <gavin.w.mead@gmail.com>
2025-12-16 13:56:30 -0500

    init resp3 protocol

---
+++ lib/gmredis/src/protocol/serialize.cpp
@@ -0,0 +1,17 @@
+#include "gmredis/protocol/serialize.h"
+
+namespace gmredis::protocol {
+    std::string serialize(const SimpleString& resp) {
+        return "+" + resp.value + "\r\n";
+    }
+
+    std::string serialize(const SimpleError& resp) {
+        return "-" + resp.value + "\r\n";
+    }
+
+    std::string serialize(const RespValue& resp) {
+        return std::visit([](const auto& value) {
+            return serialize(value);
+        }, resp);
+    }
+}  // namespace gmredis::protocol
