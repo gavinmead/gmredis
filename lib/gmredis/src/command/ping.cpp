e09f4c7b8b91743672f3c5aeff7d448884c1952a
gavin <gavin.w.mead@gmail.com>
2025-12-19 21:13:25 -0500

    Ping Command implementation

--- lib/gmredis/src/command/ping.cpp
+++ lib/gmredis/src/command/ping.cpp
@@ -1,9 +1,12 @@
 #include "gmredis/command/ping.h"
 
 namespace gmredis::command {
+    constexpr size_t MAX_PING_ARGS = 2; // command + optional message
+    constexpr size_t MESSAGE_INDEX = 1;
+
     std::optional<CommandError> PingCommand::doValidate(const protocol::Array& arg) {
         // If the array length is greater than 2, then we know it is invalid
-        if (arg.values.size() > 2) {
+        if (arg.values.size() > MAX_PING_ARGS) {
             return CommandError(CommandErrorCode::WrongArgumentCount, "ping takes 0 or 1 arguments");
         }
 
@@ -17,12 +20,11 @@ namespace gmredis::command {
     }
 
     std::expected<protocol::RespValue, CommandError> PingCommand::doExecute(const protocol::Array& arg) {
-        if (arg.values.size() == 2) {
-            return std::get<protocol::BulkString>(arg.values[1]);
+        if (arg.values.size() == MAX_PING_ARGS) {
+            const auto& bulk_str = std::get<protocol::BulkString>(arg.values[MESSAGE_INDEX]);
+            return protocol::BulkString{.value = bulk_str.value, .length = bulk_str.length};
         }
         return protocol::SimpleString{.value = "PONG"};
     }
-
-
 
 }
